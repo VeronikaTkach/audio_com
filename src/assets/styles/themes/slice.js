46de2c59ef1f6c494aeefd3567b2176a0a2362d6
Veronika <xafling@mail.ru>
2024-07-17 16:17:26 +0300

    fixed slice for Theme

--- src/assets/styles/themes/slice.js
+++ src/assets/styles/themes/slice.js
@@ -1,13 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const getInitialTheme = () => {
+  const savedTheme = localStorage.getItem('theme');
+  return savedTheme ? savedTheme : 'light';
+};
+
 export const themeSlice = createSlice({
   name: 'theme',
   initialState: {
-    mode: 'light',
+    mode: getInitialTheme(),
   },
   reducers: {
     toggleTheme: (state) => {
       state.mode = state.mode === 'light' ? 'dark' : 'light';
+      localStorage.setItem('theme', state.mode);
     },
   },
 });
