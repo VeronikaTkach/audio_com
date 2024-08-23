f1a5b7a9b09121b42644fedd5163a1388a4a920c
Veronika <xafling@mail.ru>
2024-08-23 17:18:58 +0300

    added table in AlbumPage

--- src/components/ui/AlbumDetails/AlbumDetails.jsx
+++ src/components/ui/AlbumDetails/AlbumDetails.jsx
@@ -18,17 +18,36 @@ const AlbumDetails = ({ album, user, isFavorite, onEditClick, onDeleteClick, onA
       <div className={s.album__fav}>
         <h1>{album.title}</h1>
         {user && (
-        <div className={s.favorite__icon} onClick={handleAddToFavoritesClick}>
+        <div className={s.album__fav_icon} onClick={handleAddToFavoritesClick}>
           {isFavorite ? <FaStar size={36} color="gold" /> : <FaRegStar size={36} color="gold" />}
         </div>
       )}
       </div>
       <h2>{album.artist}</h2>
-      <p><strong>Genre: </strong>{album.genre.join(', ')}</p>
-      <p><strong>Release Date:</strong> {album.release_date}</p>
-      <p><strong>Tracks:</strong> {album.value_of_tracks}</p>
-      <p><strong>Description:</strong> {album.description}</p>
-      <p><strong>Format:</strong> {album.format.join(', ')}</p>
+      <table className={s.album__table}>
+        <tbody>
+          <tr className={s.album__table_row}>
+            <td className={s.album__table_cell}>Genre:</td>
+            <td className={s.album__table_cell}>{album.genre.join(', ')}</td>
+          </tr>
+          <tr className={s.album__table_row}>
+            <td className={s.album__table_cell}>Release Date:</td>
+            <td className={s.album__table_cell}>{album.release_date}</td>
+          </tr>
+          <tr className={s.album__table_row}>
+            <td className={s.album__table_cell}>Tracks:</td>
+            <td className={s.album__table_cell}>{album.value_of_tracks}</td>
+          </tr>
+          <tr className={s.album__table_row}>
+            <td className={s.album__table_cell}>Description:</td>
+            <td className={s.album__table_cell}>{album.description}</td>
+          </tr>
+          <tr className={s.album__table_row}>
+            <td className={s.album__table_cell}>Format:</td>
+            <td className={s.album__table_cell}>{album.format.join(', ')}</td>
+          </tr>
+        </tbody>
+      </table>
       {user && user.isEditor && (
         <div className={s.album__actions}>
           <Button label="Edit" onClick={onEditClick}/>
